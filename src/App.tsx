5df28bd7227770fa4a342f496de56d9bf6996da7
Vladosik337 <vlad.ostapenko.337@gmail.com>
2024-11-05 22:46:35 +0200

    Create project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './styles/style.scss';
+
+function App() {
+  return (
+    <div className="min-h-full flex flex-col items-center justify-center overflow-clip h-screen">
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
+}
+
+export default App;
